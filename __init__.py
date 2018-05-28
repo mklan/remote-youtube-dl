bc85d9147004033dd60d50bd7cb90c62b87299a3
Matthias Klan <matthias.klan@gmail.com>
2018-05-28 15:03:33 +0200

    Update and rename index.py to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,47 +1,59 @@
 from __future__ import unicode_literals
+import os
 
 from flask import Flask
 from flask import request
 from flask import jsonify
 
 import youtube_dl
+from pbkdf2 import crypt
 
 app = Flask(__name__)
+
+HASHED_KEY = os.environ.get('YDL_HASHED_KEY', None)
+USE_AUTH = os.environ.get('YDL_USE_AUTH', False)
+DL_PATH = os.environ.get('YDL_DL_PATH', '.')
+
 
 @app.route("/", methods=["GET"])
 def handle_index():
     return handle_help()
+
 
 @app.route("/ydl", methods=["GET"])
 def handle_download():
 
     id = request.args.get('id')
     onlyAudio = request.args.get('onlyAudio')
+    key = request.args.get('key')
 
     if not id:
         return handle_help(400)
 
+    # Authentication
+    if USE_AUTH and not HASHED_KEY == crypt(key, HASHED_KEY):
+        return response(500, 'incorrect key')
+
     ydl_opts = {
         'format': 'bestaudio' if onlyAudio else 'best',
-        'outtmpl' : '~/Videos/%(title)s.%(ext)s' #TODO this needs to be configurable
+        'outtmpl': DL_PATH+'/%(title)s.%(ext)s'
     }
 
     youtube_download(id, ydl_opts)
     return response(200, 'download started...')
 
+
 @app.route("/help", methods=["GET"])
-def handle_help(code = 200):
-    return response(code, 'provide an id argument with a value of a youtube video id and an optional boolean flag onlyAudio')
-
-
+def handle_help(code=200):
+    return response(code, 'how to use this service:  /ydl?id=<YOUTUBE_ID>&key=<YOUR_SECRET>onlyAudio=false')
 
 
 def youtube_download(id, ydl_opts):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
             ydl.download(['https://www.youtube.com/watch?v='+id])
+        # TODO exception does not work due to async shizzle
         except Exception as error:
-            #TODO an error if the video was not found needs to be thrown, this does not work right now
             return response(500, 'error')
 
 
